9d0a1fbc0d8658cfba7962ecdfdb55f79e456b7d
Matěj Pešl <matejpesl1@gmail.com>
2021-05-05 23:18:32 +0200

    add functions

--- script.js
+++ script.js
@@ -71,3 +71,61 @@ function getWindowHeight() {
 function isTouchDevice() {
     return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 }
+
+
+
+
+
+
+
+
+
+// Render the PayPal button into #paypal-button-container
+paypal.Buttons({
+    // Call your server to set up the transaction
+    createOrder: function(data, actions) {
+        console.log("creating order");
+        return fetch('/netlify/functions/createOrder', {
+            method: 'post'
+        }).then(function(orderResponse) {
+            return orderResponse.json();
+        }).then(function(orderResponse) {
+            return orderResponse.orderID;
+        });
+    },
+
+    // Call your server to finalize the transaction
+    onApprove: function(orderResponse, actions) {
+        console.log("finalizing order");
+        return fetch('/netlify/functions/captureOrder&orderID=' + orderResponse.orderID, {
+            method: 'post'
+        }).then(function(response) {
+            return response.json();
+        }).then(function(orderData) {
+            // Three cases to handle:
+            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+            //   (2) Other non-recoverable errors -> Show a failure message
+            //   (3) Successful transaction -> Show confirmation or thank you
+
+            // This example reads a v2/checkout/orders capture response, propagated from the server
+            // You could use a different API or structure for your 'orderData'
+            var errorDetail = Array.isArray(orderData.details) && orderData.details[0];
+
+            if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+                return actions.restart(); // Recoverable state, per:
+                // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
+            }
+
+            if (errorDetail) {
+                var msg = 'Sorry, your transaction could not be processed.';
+                if (errorDetail.description) msg += '\n\n' + errorDetail.description;
+                if (orderData.debug_id) msg += ' (' + orderData.debug_id + ')';
+                return alert(msg); // Show a failure message
+            }
+
+            // Show a success message
+            alert('Transaction completed by ' + orderData.payer.name.given_name);
+        });
+    }
+
+}).render('#paypal-button-container');
