8d7f5834a056ccae900d7faf64213a81ffc0db7f
Matěj Pešl <matejpesl1@gmail.com>
2021-05-08 10:42:36 +0200

    add .gitignore

---
+++ functions/transactionProcessor.js
@@ -0,0 +1,70 @@
+'use strict';
+const paypal = require('@paypal/checkout-server-sdk');
+/* Returns PayPal HTTP client instance with environment that has access credentials context.
+   Use this instance to invoke PayPal APIs, provided the credentials have access. */
+const client = () => new paypal.core.PayPalHttpClient(environment());
+// Set up and return PayPal JavaScript SDK environment with PayPal access credentials.
+const environment = () => process.env.SANDBOX
+    ? new paypal.core.SandboxEnvironment(process.env.SANDBOX_CLIENT_ID, process.env.SANDBOX_CLIENT_SECRET)
+    : new paypal.core.LiveEnvironment(process.env.LIVE_CLIENT_ID, process.env.LIVE_CLIENT_SECRET);
+
+exports.handler = async (event, context) => {
+    const orderAction = event.queryStringParameters.orderAction;
+    const orderId = event.queryStringParameters.orderID;
+
+    if (orderAction == 'create')
+        return createOrder(client());
+
+    if (orderAction == 'capture') {
+        return orderId
+            ? captureOrder(client(), orderId)
+            : { statusCode: 400, body: JSON.stringify({error: 'no order ID passed.'}) };
+    }
+
+    return { statusCode: 400, body: JSON.stringify({error: 'no action specified or bad format'}) }
+}
+
+async function createOrder(client) {
+    // Call PayPal to set up a transaction.
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer('return=minimal');
+    request.requestBody({
+        intent: 'CAPTURE',
+        purchase_units: [{
+            amount: {
+                currency_code: 'CZK',
+                value: '49.99'
+            }
+        }]
+    });
+
+    let serverResponse;
+    try {
+        serverResponse = await client.execute(request);
+        console.log('order successfuly created');
+        // Return a successful serverResponse to the client with the order ID.
+        return { statusCode: 200, body: JSON.stringify({ serverResponse: serverResponse, id: serverResponse.result.id }) }
+    } catch (err) {
+        console.error("Could not create order. Err: " + err);
+        return { statusCode: 500, body: JSON.stringify({error: err}) }
+    }
+}
+
+async function captureOrder(client, orderId) {
+    // Call PayPal to capture the order.
+    const request = new paypal.orders.OrdersCaptureRequest(orderId);
+    request.requestBody({});
+    console.log("Capturing order. Id: " + orderId);
+
+    try {
+        let serverResponse = await client.execute(request);
+        console.log('successfuly captured order');
+        return { statusCode: 200, body: JSON.stringify({ serverResponse: serverResponse, key: '[a key will be here]' }) }
+        // Save the capture ID to your database. Implement logic to save capture to your database for future reference.
+        // const captureID = capture.result.purchase_units[0].payments.captures[0].id;
+        // await database.saveCaptureID(captureID);
+    } catch (err) {
+        console.error("Could not capture order. Err: " + err);
+        return { statusCode: 500, body: JSON.stringify({error: err}) }
+    }
+}
