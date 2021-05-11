ef1558351154e3a1e3a8518486aa84880ce640db
Matěj Pešl <matejpesl1@gmail.com>
2021-05-11 19:50:42 +0200

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,10 @@ import Parallaxed from "./parallax.js";
 import AOS from "aos";
 import setUp from "./paypalButtonSetup.js";
 
-window.onload = () => AOS.init({ duration: 1000, once: true });
+window.onload = () => {setUp();}
 
 document.addEventListener("DOMContentLoaded", () => {
     if (!isTouchDevice())
         new Parallaxed(document.getElementById("header-icon"), 6, 0).makeParallaxed();
+    AOS.init({duration: 1000, once: true});
 });
-
-setUp();
